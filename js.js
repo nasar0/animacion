10f1102ade03f799f812f62043330fbfaf505ce8
nasrallah <nasaroanas6@gmail.com>
2024-11-09 14:37:41 +0100

    2024-11-09

---
+++ js.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let soldado = document.querySelector("#soldado")
+    setTimeout(function() {
+        soldado.style.backgroundPosition = "-128px 0"; 
+    }, 2000);
+})
